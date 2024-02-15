09a7651b6b7605d0653d2fd91c492f378d163f4a
JaiderMartinez <smith.martinez@pragma.com.co>
2024-02-15 10:56:21 -0500

    se realiza configuracion para el despliegue del microservicio

--- infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/co/com/parking/r2dbc/repository/ParkingRepositoryImplTest.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/test/java/co/com/parking/r2dbc/repository/ParkingRepositoryImplTest.java
@@ -1,34 +1,48 @@
 package co.com.parking.r2dbc.repository;
 
+import co.com.parking.model.parking.Parking;
 import co.com.parking.r2dbc.dao.ParkingDao;
 import co.com.parking.r2dbc.entities.ParkingEntity;
 import co.com.parking.r2dbc.mapper.ParkingMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ParkingRepositoryImplTest {
 
     private static final String VALUE_PROPERTIES_LIMIT_PARKING_LOTS_BY_LOCATION = "2";
-    @Mock
     private ParkingDao parkingDao;
-    @InjectMocks
     private ParkingRepositoryImpl parkingRepository;
+    private ParkingEntity parkingEntityExpected;
+
+    @BeforeEach
+    void setUp() {
+        parkingDao = mock(ParkingDao.class);
+        parkingRepository = new ParkingRepositoryImpl(VALUE_PROPERTIES_LIMIT_PARKING_LOTS_BY_LOCATION, parkingDao);
+        parkingEntityExpected = ParkingEntity.builder()
+                .id(1L)
+                .address("address")
+                .name("name")
+                .hourPrice(1000)
+                .latitude(10)
+                .longitude(-10)
+                .build();
+    }
 
     @Test
     void shouldSaveSuccessTest() {
-        ParkingEntity parkingEntityExpected = new ParkingEntity(1L, "Parking 1", 1000, "Calle 2", -10, 10);
-        when(parkingDao.save(argThat( (parkingEntityRequest) -> parkingEntityRequest.getName().equals(parkingEntityExpected.getName())) )
-        ).thenReturn(Mono.just(parkingEntityExpected));
+        when(parkingDao.save(argThat( (parkingEntityRequest) ->
+                parkingEntityRequest.getName().equals(parkingEntityExpected.getName())) ))
+                .thenReturn(Mono.just(parkingEntityExpected));
         StepVerifier.create(parkingRepository.save(ParkingMapper.toModel(parkingEntityExpected)))
                 .expectNextMatches(parkingResult -> parkingResult.getName().equals(parkingEntityExpected.getName()))
                 .verifyComplete();
@@ -36,17 +50,13 @@ class ParkingRepositoryImplTest {
 
     @Test
     void shouldFindAllSuccessTest() {
-        ParkingEntity parkingEntityExpected = new ParkingEntity(1L, "Parking 1", 1000, "Calle 2", -10, 10);
         when(parkingDao.findAll()).thenReturn(Flux.just(parkingEntityExpected));
-        StepVerifier.create(parkingRepository.findAll())
-                .expectNextMatches(parkingResult -> parkingResult.getName().equals(parkingEntityExpected.getName()))
-                .verifyComplete();
+        assertMatches(parkingRepository.findAll());
     }
 
     @Test
     void shouldFindByIdSuccessTest() {
         Long idParking = 1L;
-        ParkingEntity parkingEntityExpected = new ParkingEntity(1L, "Parking 1", 1000, "Calle 2", -10, 10);
         when(parkingDao.findById(idParking)).thenReturn(Mono.just(parkingEntityExpected));
         StepVerifier.create(parkingRepository.findById(idParking))
                 .expectNextMatches(parkingResult -> parkingResult.getName().equals(parkingEntityExpected.getName()))
@@ -57,11 +67,24 @@ class ParkingRepositoryImplTest {
     void shouldFindByLocationSuccessTest() {
         double latitudeInRadians = -49;
         double longitudeInRadians = 4;
-        ParkingEntity parkingEntityExpected = new ParkingEntity(1L, "Parking 1", 1000, "Calle 2",
-                Math.toDegrees(latitudeInRadians), Math.toDegrees(longitudeInRadians));
-        when(parkingDao.findByLocation(latitudeInRadians, longitudeInRadians, Integer.parseInt(VALUE_PROPERTIES_LIMIT_PARKING_LOTS_BY_LOCATION))).thenReturn(Flux.just(parkingEntityExpected));
-        StepVerifier.create(parkingRepository.findByLocation(latitudeInRadians, longitudeInRadians))
-                .expectNext(ParkingMapper.toModel(parkingEntityExpected))
+        parkingEntityExpected.setLongitude(Math.toDegrees(longitudeInRadians));
+        parkingEntityExpected.setLatitude(Math.toDegrees(latitudeInRadians));
+        when(parkingDao.findByLocation(latitudeInRadians, longitudeInRadians, Integer.parseInt(VALUE_PROPERTIES_LIMIT_PARKING_LOTS_BY_LOCATION)))
+                .thenReturn(Flux.just(parkingEntityExpected));
+        assertMatches(parkingRepository.findByLocation(latitudeInRadians, longitudeInRadians));
+    }
+
+    private void assertMatches(Flux<Parking> parkings) {
+        StepVerifier.create(parkings)
+                .expectNextMatches(
+                        parking ->
+                                parking.getId().equals(parkingEntityExpected.getId()) &&
+                                parking.getName().equals(parkingEntityExpected.getName()) &&
+                                parking.getAddress().equals(parkingEntityExpected.getAddress()) &&
+                                parking.getHourPrice() == parkingEntityExpected.getHourPrice() &&
+                                parking.getLatitude() == parkingEntityExpected.getLatitude() &&
+                                parking.getLongitude() == parkingEntityExpected.getLongitude()
+                )
                 .verifyComplete();
     }
 }
